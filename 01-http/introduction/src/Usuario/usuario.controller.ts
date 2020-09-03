f7deb0fea9e13214d4899d7707898857d93773b6
Gaby Talavera <gabriela.talavera@epn.edu.ec>
2020-09-03 16:36:09 -0500

    Realizar formularios en las vistas, toma informacion de la base de datos

--- 01-http/introduction/src/Usuario/usuario.controller.ts
+++ 01-http/introduction/src/Usuario/usuario.controller.ts
@@ -4,15 +4,16 @@ import {
     Controller,
     Delete,
     Get,
-    InternalServerErrorException,
+    InternalServerErrorException, NotFoundException,
     Param,
     Post,
-    Put
+    Put, Res
 } from "@nestjs/common";
 import {UsuarioService} from "./usuario.service";
-import objectContaining = jasmine.objectContaining;
 import {UsuarioCreateDto} from "./dto/usuario.create-dto";
 import {UsuarioUpdateDto} from "./dto/usuario.update-dto";
+import {MascotaService} from "../mascota/mascota.service";
+
 
 @Controller('usuario')
 
@@ -32,7 +33,8 @@ export class UsuarioController {
 
 
     constructor(//inyeccion de dependencias)
-     private readonly _usuarioService: UsuarioService
+     private readonly _usuarioService: UsuarioService,
+    private readonly _mascotaService: MascotaService
     ){
 
     }
@@ -180,6 +182,106 @@ export class UsuarioController {
         return this.arregloUsuarios[indice];
  */
     }
+    @Post ('crearUsuarioYCrearMascota')
+    async crearUsuarioYCrearMascota(
+        @Body() parametrosCuerpo
+    ){
+        const usuario= parametrosCuerpo.usuario;
+        const mascota = parametrosCuerpo.mascota;
+        //validar mascota
+        //validar usuario
+        //creamos los dos
+        let usuarioCreado;
+        try {
+            usuarioCreado = await this._usuarioService.crearUno(usuario);
+        }catch (e) {
+            console.error(e);
+            throw new BadRequestException({
+                mensaje: 'Error creando usuario',
+            })
+        }
+        if(usuarioCreado){
+            mascota.usuario = usuarioCreado.id;
+            let mascotaCreada;
+            try {
+                mascotaCreada = await this._mascotaService.crearNuevaMascota(mascota);
+            }catch (e){
+                console.error(e);
+                throw new BadRequestException({
+                    mensaje:'Error al crear Mascota',
+                })
+            }
+        if(mascotaCreada) {
+            return {
+                mascota: mascotaCreada,
+                usuario: usuarioCreado
+            }
+        }else{
+                throw new InternalServerErrorException({
+                    mensaje:'Error al crear Mascota',
+                })
+            }
+        }else{
+            throw new InternalServerErrorException({
+                mensaje:'Error al crear Mascota',
+            })
+        }
+
+    }
+
+    @Get('vista/usuario')
+    vistaUsuario(
+        @Res() res
+    ){
+        const nombreControlador ='Gaby';
+        res.render(
+            'usuario/ejemplo',//nombre de la vista
+        {//Parametros de la vista
+            nombre: nombreControlador,
+        })
+    }
+
+    @Get('vista/faq')
+faq(
+    @Res() res
+    ){
+        res.render('usuario/faq')
+    }
+
+    @Get('vista/inicio')
+    async inicio(
+        @Res() res
+    ){
+        let resultadoEncontrado
+        try {
+            resultadoEncontrado = await this._usuarioService.buscarTodos();
+        }catch (error) {
+         throw new  InternalServerErrorException('Error encontrando usuarios')
+                    }
+        if(resultadoEncontrado) {
+            res.render('usuario/inicio',{
+                arregloUsuarios: resultadoEncontrado
+            });
+        }else{
+        throw new NotFoundException('No se encontraron usuarios ')
+    }
+    }
+
+    @Get('vista/crear')
+    crearUsuarioVista(
+        @Res() res
+
+    ){
+        res.render('usuario/crear')
+    }
+
+@Get('vista/login')
+    login(
+        @Res() res
+    ){
+        res.render('usuario/login')
+    }
+
 }
 
 //XML <usuario>GABY</nombre></usuario>
--- 01-http/introduction/src/main.ts
+++ 01-http/introduction/src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module'; //Importar cosas en TS
 const cookieParser = require('cookie-parser'); //Importar cosas en JS
+const express = require('express');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule) as any;
   //LAS CONFIGURACIONES SE REALIZAN EN ESTA AREA
   //await app.listen(3001);
-  app.use(cookieParser('%%EXAMEN-WEB%%'))
+  app.use(cookieParser('%%EXAMEN-WEB%%'));
+  app.set('view engine','ejs')
+  app.use(express.static('publico'))
   await app.listen(3001);
 }
+
 bootstrap();
