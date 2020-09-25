78d8dd4ccbeacc0aa46f30430c6c6049efea303d
Gaby Talavera <gabriela.talavera@epn.edu.ec>
2020-09-24 23:00:04 -0500

    Hasta Seguridad de sesiones

--- 01-http/introduction/src/app.controller.ts
+++ 01-http/introduction/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import {Body, Controller, Get, Post, Req, Res, Session} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,65 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get('login')
+  login(
+      @Res() response
+  ){
+    return response.render('login/login')
+  }
+
+  @Post('login')
+  loginPost(
+      @Body() parametrosConsulta,
+      @Res() response,
+      @Session() session
+  ) {
+    // validamos datos
+    const usuario = parametrosConsulta.usuario;
+    const password = parametrosConsulta.password;
+    if (usuario == 'Gab' && password == '1234') {
+      session.usuario = usuario
+      session.roles = ['Administrador']
+      return response.redirect('protegido');
+    } else {
+      if (usuario == 'Bayola' && password == '4321') {
+        session.usuario = usuario
+        session.roles = ['Supervisor']
+        return response.redirect('protegido');
+      } else {
+        return response.redirect('/login')
+      }
+    }
+  }
+
+  @Get('protegido')
+  protegido(
+      @Res() response,
+      @Session() session,
+  ) {
+    const estaLogeado = session.usuario;
+    if (estaLogeado) {
+      return response.render(
+          'login/protegido',
+          {
+            usuario: session.usuario,
+            roles: session.roles
+          }
+      )
+    } else {
+      return response.redirect('/login')
+    }
+  }
+
+  @Get('logout')
+  logout(
+      @Session() session,
+      @Res() response,
+      @Req() request
+  ){
+    session.username = undefined
+    session.roles= undefined
+    request.session.destroy('login')
+  }
+
 }
